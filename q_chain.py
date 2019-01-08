d160aba187142d9903eead9b12f638869d13e246
smbody-mipt <baal.buchatsky@gmail.com>
2019-01-08 18:20:09 +0300

    naive attempt to perform q remote calls in a separate thread

--- q_chain.py
+++ q_chain.py
@@ -1,5 +1,7 @@
 import sublime
 import sublime_plugin
+import threading
+import re
 
 #chain command and parse output to next command inputs
 #example
@@ -7,6 +9,7 @@ import sublime_plugin
 #view.run_command("q_chain", {"input": "til 10", "chain": ["q_send", "q_out_panel"]})
 #view.run_command("q_chain", {"input": "test output", "chain": ["q_out_panel"]})
 class QChainCommand(sublime_plugin.TextCommand):
+
     def do(self, edit, input=None):
         if input is not None:
             return input
@@ -14,6 +17,14 @@ class QChainCommand(sublime_plugin.TextCommand):
             return "start"  #to start chain
 
     def run(self, edit, input=None, chain=None):
+        if (len(chain) > 0 and re.search("QSend.*", self.__class__.__name__)):
+            print("Spawning thread for q send action "+self.__class__.__name__)
+            t = QChainCommandThread(self, edit, input, chain)
+            t.start()
+        else:
+            self.exec_chain(edit, input, chain)
+
+    def exec_chain(self, edit, input, chain):
         output = self.do(edit, input)
         if output is None:
             print('break chain because output is none')
@@ -29,6 +40,14 @@ class QChainCommand(sublime_plugin.TextCommand):
             #no chain - just print output to console
             print(output)
 
-
-
-  
+class QChainCommandThread(threading.Thread):
+    def __init__(self, cmd, edit, input, chain):
+        print("thread init")
+        self.cmd = cmd
+        self.edit = edit
+        self.input = input
+        self.chain = chain
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        self.cmd.exec_chain(self.edit, self.input, self.chain)
